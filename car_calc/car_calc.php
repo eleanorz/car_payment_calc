d99fd3a322643e4b82e544e8919381ca0f08fcd9
elly <eleanorz@gmail.com>
2014-01-05 17:26:41 -0800

    continued formatting of buttons and nav bars

--- car_calc/car_calc.php
+++ car_calc/car_calc.php
@@ -9,57 +9,48 @@
 	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
 	<script src="../../bootstrap/docs/assets/js/bootstrap.min.js"></script>
-	<link href='http://fonts.googleapis.com/css?family=Ewert' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type='html/css' href="car_calc.css">
-	
-	<!-- Below enables dragability of images -->
-	<script>
-		$(document).ready(function()
-			{
-				$( '.img-polaroid' ).resizable();
-				$( '.img-polaroid' ).draggable();
-			});
-  	</script>
-	
-
 </head>
+
 <body>
-	<div id='top_nav'>
-<!-- 		<h1>Car Haggler</h1> -->		
-<img id= 'logo' src="http://www.logomaker.com/logo-images/9847677fc7a8eec4.gif"/>
-<!-- 
-<img src="http://www.logomaker.com/logo-images/9847677fc7a8eec4.gif"/>
-<a href="http://www.logomaker.com"><img src="http://www.logomaker.com/images/logos.gif" alt="logos" border="0"/></a> -->
+	<div id='top_nav'>	
+		<img id= 'logo' src="http://www.logomaker.com/logo-images/9847677fc7a8eec4.gif"/>
+
+		<div id = 'login'>
+			<input type="text" name = 'email' placeholder = 'email'>	
+			<input type="text" name = 'password' placeholder = 'password'>
+			<input type="submit" class = 'btn btn-default btn-xs' value='login'>
+			<br><a href="cnn.com">new user?</a>
+		</div>
+			
 	</div>
 
 	<div id='center_shell'>
 			<!--below, is the form for submitting a new dealer offer -->		
-			<form action= 'car_calc_process.php' method = 'post'>
-			<input type="text" name = 'dealer' placeholder = 'Dealer'>
+			<form id = 'add_new' action= 'car_calc_process.php' method = 'post'>
+			<h4>Add New Offer</h4>
+			<input type="text" name = 'dealer' placeholder = 'dealer'>
 			<br>
-			<input type="text" name = 'car_type' placeholder = 'Car Type'>
+			<input type="text" name = 'car_type' placeholder = 'car type'>
 			<br>
-			<input type="text" name = 'monthly_payment' placeholder='Monthly Payment'>
+			<input type="text" name = 'monthly_payment' placeholder='monthly payment'>
 			<br>
-			<input type="text" name = 'num_months' placeholder='Number of Months'>
-			OR
-			<input type="text" name = 'num_years' placeholder='Number of Years'>
+			<input type="text" name = 'num_months' placeholder='number of months'>
 			<br>
-			<input type="text" name = 'apr' placeholder='APR %'>
+			<input type="text" name = 'num_years' placeholder='or number of years'>
 			<br>
-			<input type="text" name = 'down_payment' placeholder='Down Payment %'>
+			<input type="text" name = 'apr' placeholder='APR'>
 			<br>
-			<input type="text" name = 'sec_dep' placeholder='Security Deposit %'>
+			<input type="text" name = 'down_payment' placeholder='down payment'>
+			<br>
+			<input type="text" name = 'sec_dep' placeholder='security deposit'>
 			<br>
 			<input type = 'hidden' name = 'action' value = 'add_calc'>
-			<input type="submit" class="btn btn-success" value='Calculate NPV'>
+			<input type="submit" class="btn btn-primary btn-lg" value='calculate !'>
 		</form>
 		<!-- End of adding new dealer offer Form -->
 
 	</div>
-	
-	
-	
 
 
 
